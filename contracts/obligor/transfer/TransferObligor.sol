0b32221340d414b1a72117a34be84b26b913ef4e
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-05-15 21:20:54 +0400

    Get rid of asset permitter in transfer obligor

--- contracts/obligor/transfer/TransferObligor.sol
+++ contracts/obligor/transfer/TransferObligor.sol
@@ -3,18 +3,14 @@
 pragma solidity ^0.8.26;
 
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {Multicall} from "@openzeppelin/contracts/utils/Multicall.sol";
-
-import {AssetPermitter} from "../../permit/AssetPermitter.sol";
 
 import {PoolObligor, IFlexPool} from "../pool/PoolObligor.sol";
 
 import {ITransferObligor} from "./interfaces/ITransferObligor.sol";
 
-contract TransferObligor is ITransferObligor, PoolObligor, AssetPermitter, Multicall {
+contract TransferObligor is ITransferObligor, PoolObligor {
     constructor(IFlexPool pool_)
         PoolObligor(pool_)
-        AssetPermitter(poolAsset)
     {}
 
     function _obligate(
--- contracts/obligor/transfer/interfaces/ITransferObligor.sol
+++ contracts/obligor/transfer/interfaces/ITransferObligor.sol
@@ -2,10 +2,8 @@
 
 pragma solidity ^0.8.26;
 
-import {IAssetPermitter} from "../../../permit/AssetPermitter.sol";
-
 import {IPoolObligor} from "../../pool/interfaces/IPoolObligor.sol";
 
-interface ITransferObligor is IPoolObligor, IAssetPermitter {
+interface ITransferObligor is IPoolObligor {
     error InsufficientTransfer(uint256 assets, uint256 minAssets);
 }

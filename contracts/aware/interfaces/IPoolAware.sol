f4ac4f8e2700eee437d3a367bc987cffe9337aa2
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-05-26 14:52:30 +0400

    Use interface OZ file in pool aware

--- contracts/aware/interfaces/IPoolAware.sol
+++ contracts/aware/interfaces/IPoolAware.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.26;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 import {IFlexPool} from "../../pool/interfaces/IFlexPool.sol";
 
